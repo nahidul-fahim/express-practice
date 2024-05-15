38868aacb87e31c4f91a6df7106195d1a75671f3
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-05-15 23:54:56 +0600

    using express router

--- dist/app.js
+++ dist/app.js
@@ -7,11 +7,37 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 // parsers
 app.use(express_1.default.json());
+// router
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+// then we have to use the routers that we have created
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+// 
+userRouter.get("/create-user", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    res.json({
+        success: true,
+        message: "User is created!"
+    });
+});
+courseRouter.post("/create-course", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    res.json({
+        success: true,
+        message: "New course is loading!"
+    });
+});
 app.get('/', (req, res) => {
     res.send('Hello world! This is Nahid calling.');
 });
+// post request
 app.post("/", (req, res) => {
     console.log(req.body);
-    res.send("Got the data");
+    res.json({
+        message: "successfully got the data"
+    });
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,46 @@ const app = express();
 // parsers
 app.use(express.json())
 
+
+// router
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+// then we have to use the routers that we have created
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+
+// 
+userRouter.get("/create-user", (req: Request, res: Response) => {
+    const data = req.body;
+    console.log(data)
+    res.json({
+        success: true,
+        message: "User is created!"
+    })
+})
+
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+    const data = req.body;
+    console.log(data)
+    res.json({
+        success: true,
+        message: "New course is loading!"
+    })
+})
+
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello world! This is Nahid calling.')
 })
 
+// post request
 app.post("/", (req: Request, res: Response) => {
     console.log(req.body);
-    res.send("Got the data")
+    res.json({
+        message: "successfully got the data"
+    })
 })
 
 export default app;
